64a3b335a841b86ed5671606b40c8a8e21c8f9bc
Denis Skripov <nizhikebinesi@gmail.com>
2022-06-25 19:36:42 +0600

    handling encoding troubles, start getting general information

--- scraper/main.go
+++ scraper/main.go
@@ -19,7 +19,6 @@ type ReaderType interface {
 	Read(p []byte) (n int, err error)
 }
 
-
 func main() {
 	userAgent := "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
 	accept := "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
